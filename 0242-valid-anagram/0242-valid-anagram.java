b0b192585c8ecb9ce14f3a21ff5fdb04b99a919e
Tanish vyas <vyastanish112@gmail.com>
2026-07-11 10:46:48 +0530

    Time: 17 ms (16.66%), Space: 46.6 MB (24.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,9 +1,28 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        // if(s.length()!=t.length()){
+        //     return false;
+        // }
+        // HashMap<Character , Integer> map = new HashMap<>();
+        // for(char ch : s.toCharArray()){
+        //     map.put(ch,map.getOrDefault(ch,0)+1);
+        // }
+        // for(char ch : t.toCharArray()){
+        //     if(!map.containsKey(ch)){
+        //         return false;
+        //     }
+        //     map.put(ch,map.get(ch)-1);
+        //     if(map.get(ch)==0){
+        //         map.remove(ch);
+        //     }
+        // }
+        // return map.isEmpty();
+
+
         if(s.length()!=t.length()){
             return false;
         }
-        HashMap<Character , Integer> map = new HashMap<>();
+        HashMap<Character,Integer> map = new HashMap<>();
         for(char ch : s.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
@@ -15,10 +34,12 @@ class Solution {
             if(map.get(ch)==0){
                 map.remove(ch);
             }
-
-
-
+           
         }
-        return map.isEmpty();
+
+
+ return map.isEmpty();
+
+
     }
 }

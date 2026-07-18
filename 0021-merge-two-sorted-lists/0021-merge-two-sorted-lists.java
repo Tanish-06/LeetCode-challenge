e99c0ef9e2dc7e8eae831d6f9cb32c88e55c9339
Tanish vyas <vyastanish112@gmail.com>
2026-07-18 12:38:35 +0530

    Time: 0 ms (100%), Space: 44.2 MB (57.93%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,32 +10,56 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-        ListNode head = new ListNode(1000);
-        ListNode temp = head;
+        // ListNode temp1 = list1;
+        // ListNode temp2 = list2;
+        // ListNode head = new ListNode(1000);
+        // ListNode temp = head;
 
-        while(temp1!=null && temp2!=null){
-            if(temp1.val<temp2.val){
-                ListNode a = new ListNode(temp1.val);
-                temp.next = a;
-                temp = a;
-                temp1 = temp1.next;
-            }
-             else{
-                ListNode a = new ListNode(temp2.val);
-                temp.next = a;
-                temp = a;
-                temp2 = temp2.next;
-            }
+        // while(temp1!=null && temp2!=null){
+        //     if(temp1.val<temp2.val){
+        //         ListNode a = new ListNode(temp1.val);
+        //         temp.next = a;
+        //         temp = a;
+        //         temp1 = temp1.next;
+        //     }
+        //      else{
+        //         ListNode a = new ListNode(temp2.val);
+        //         temp.next = a;
+        //         temp = a;
+        //         temp2 = temp2.next;
+        //     }
           
+        // }
+        //   if(temp1!=null){
+        //         temp.next=temp1;
+        //     }
+        //      else{
+        //         temp.next=temp2;
+        //     }
+        // return head.next;
+
+
+
+        ListNode t1 = list1;
+        ListNode t2 = list2;
+        ListNode h = new ListNode(1000);
+        ListNode t = h;
+        while(t1!=null && t2!=null){
+            if(t1.val<t2.val){
+                t.next = t1;
+                t = t1;
+                t1 = t1.next;
+            }else{
+                t.next = t2;
+                t = t2;
+                t2 = t2.next;
+            }
         }
-          if(temp1!=null){
-                temp.next=temp1;
-            }
-             else{
-                temp.next=temp2;
-            }
-        return head.next;
+        if(t1==null){
+            t.next = t2;
+        }else{
+            t.next = t1;
+        }
+        return h.next;
     }
 }
